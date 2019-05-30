9af91a907771582dee7686b4363d28e8aabd7d80
crrpla00 <paolo.cerrito@uniroma2.it>
2019-05-30 12:18:30 +0200

    modificato la lettura della pem-dir in maniera da fare lo split di una eventuale lista di directory da caricare tramite ;

--- src/configuration.c
+++ src/configuration.c
@@ -1136,49 +1136,54 @@ config_disp_log_facility (int facility)
 }
 
 int
-config_scan_pem_dir(char *pemdir, hitch_config *cfg)
+config_scan_pem_dir(char *pemdirin, hitch_config *cfg)
 {
 	int n, i;
 	struct dirent **d;
 
-	n = scandir(pemdir, &d, NULL, alphasort);
-	if (n < 0) {
-		config_error_set("Unable to open directory '%s': %s", pemdir,
-		    strerror(errno));
-		return (1);
-	}
-	for (i = 0; i < n; i++) {
-		struct cfg_cert_file *cert;
-		char fpath[PATH_MAX + NAME_MAX];
+	char *pemdir = strtok(pemdirin, ";");
 
-		if (cfg->PEM_DIR_GLOB != NULL) {
-			if (fnmatch(cfg->PEM_DIR_GLOB, d[i]->d_name, 0))
+	while(pemdir != NULL) {
+		n = scandir(pemdir, &d, NULL, alphasort);
+		if (n < 0) {
+			config_error_set("Unable to open directory '%s': %s", pemdir,
+			    strerror(errno));
+			return (1);
+		}
+		for (i = 0; i < n; i++) {
+			struct cfg_cert_file *cert;
+			char fpath[PATH_MAX + NAME_MAX];
+	
+			if (cfg->PEM_DIR_GLOB != NULL) {
+				if (fnmatch(cfg->PEM_DIR_GLOB, d[i]->d_name, 0))
+					continue;
+			}
+			if (d[i]->d_type != DT_REG)
 				continue;
+	
+			strncpy(fpath, pemdir, PATH_MAX + NAME_MAX - 1);
+			strncat(fpath, d[i]->d_name,
+			    PATH_MAX + NAME_MAX - strlen(fpath) - 1);
+	
+			cert = cfg_cert_file_new();
+			config_assign_str(&cert->filename, fpath);
+			int r = cfg_cert_vfy(cert);
+			if (r != 0) {
+				/* If no default has been set, use the first
+				 * match according to alphasort  */
+				if (cfg->CERT_DEFAULT == NULL)
+					cfg->CERT_DEFAULT = cert;
+				else
+					cfg_cert_add(cert, &cfg->CERT_FILES);
+			} else {
+				cfg_cert_file_free(&cert);
+			}
+			free(d[i]);
 		}
-		if (d[i]->d_type != DT_REG)
-			continue;
-
-		strncpy(fpath, pemdir, PATH_MAX + NAME_MAX - 1);
-		strncat(fpath, d[i]->d_name,
-		    PATH_MAX + NAME_MAX - strlen(fpath) - 1);
-
-		cert = cfg_cert_file_new();
-		config_assign_str(&cert->filename, fpath);
-		int r = cfg_cert_vfy(cert);
-		if (r != 0) {
-			/* If no default has been set, use the first
-			 * match according to alphasort  */
-			if (cfg->CERT_DEFAULT == NULL)
-				cfg->CERT_DEFAULT = cert;
-			else
-				cfg_cert_add(cert, &cfg->CERT_FILES);
-		} else {
-			cfg_cert_file_free(&cert);
-		}
-		free(d[i]);
+	
+		free(d);
+		pemdir = strtok(NULL, ";");
 	}
-
-	free(d);
 	return (0);
 }
 
